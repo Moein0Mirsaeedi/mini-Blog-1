f0b4625421485a3318a216d53b73166938434f19
Moein Mirsaeedi <moein.mirsaeedi@gmail.com>
2023-07-18 17:00:50 +0330

    Add profile section and update home

--- assets/js/pages/dashboard.js
+++ assets/js/pages/dashboard.js
@@ -1,4 +1,5 @@
 let menuIcons = document.querySelectorAll(".icons-menu");
+let sectionPages = document.querySelectorAll(".sectionPages");
 
 let parentPages = document.getElementById("parentPages");
 parentPages.style.transform = `translateX(-200vw)`;
@@ -16,6 +17,12 @@ menuIcons.forEach((element) => {
   element.addEventListener("click", (event) => {
     locationPages.forEach((pages) => {
       if (pages.name == event.target.id) {
+        sectionPages.forEach((element) => {
+          if (element.id == pages.name) {
+            sizePage(element);
+          }
+        });
+        document.getElementById("titleSite").innerHTML = `Dashboard | ${pages.name}`;
         movePage(pages);
         menuIcons.forEach((element) => {
           element.classList.remove("active-icon");
@@ -28,5 +35,9 @@ menuIcons.forEach((element) => {
 
 function movePage(locationMove) {
   parentPages.style.transform = `translateX(-${locationMove.location}vw)`;
-  parentPages.style.height = `${locationMove.height}px`;
+}
+
+function sizePage(size) {
+  console.log(size.children[0].clientHeight);
+  parentPages.style.height = `${size.children[0].clientHeight + 100}px`;
 }
