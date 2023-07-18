7ba8d21e2ac5f2a95c6c6f08a577372851039dde
Moein Mirsaeedi <moein.mirsaeedi@gmail.com>
2023-07-18 15:36:15 +0330

    Update

--- assets/js/pages/dashboard.js
+++ assets/js/pages/dashboard.js
@@ -2,20 +2,21 @@ let menuIcons = document.querySelectorAll(".icons-menu");
 
 let parentPages = document.getElementById("parentPages");
 parentPages.style.transform = `translateX(-200vw)`;
+parentPages.style.height = `870px`;
 
 let locationPages = [
-  { name: "setting", location: "0" },
-  { name: "profile", location: "100" },
-  { name: "home", location: "200" },
-  { name: "activity", location: "300" },
-  { name: "blog", location: "400" },
+  { name: "setting", location: "0", height: "" },
+  { name: "profile", location: "100", height: "" },
+  { name: "home", location: "200", height: "870" },
+  { name: "activity", location: "300", height: "" },
+  { name: "blog", location: "400", height: "" },
 ];
 
 menuIcons.forEach((element) => {
   element.addEventListener("click", (event) => {
     locationPages.forEach((pages) => {
       if (pages.name == event.target.id) {
-        movePage(pages.location);
+        movePage(pages);
         menuIcons.forEach((element) => {
           element.classList.remove("active-icon");
         });
@@ -26,5 +27,6 @@ menuIcons.forEach((element) => {
 });
 
 function movePage(locationMove) {
-  parentPages.style.transform = `translateX(-${locationMove}vw)`;
+  parentPages.style.transform = `translateX(-${locationMove.location}vw)`;
+  parentPages.style.height = `${locationMove.height}px`;
 }
