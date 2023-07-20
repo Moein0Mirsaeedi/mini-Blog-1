6b274f482ed784f2bd3af0c094bcbea67ae30d83
Moein Mirsaeedi <moein.mirsaeedi@gmail.com>
2023-07-20 20:05:49 +0330

    Add settings section and update

--- assets/js/pages/dashboard.js
+++ assets/js/pages/dashboard.js
@@ -2,32 +2,33 @@ let menuIcons = document.querySelectorAll(".icons-menu");
 let sectionPages = document.querySelectorAll(".sectionPages");
 
 let parentPages = document.getElementById("parentPages");
-parentPages.style.transform = `translateX(-200vw)`;
+parentPages.style.transform = `translateX(-00vw)`;
 parentPages.style.height = `870px`;
 
 let locationPages = [
-  { name: "setting", location: "0", height: "" },
-  { name: "profile", location: "100", height: "" },
-  { name: "home", location: "200", height: "870" },
-  { name: "activity", location: "300", height: "" },
-  { name: "blog", location: "400", height: "" },
+  { name: "setting", location: "0",},
+  { name: "profile", location: "100",},
+  { name: "home", location: "200",},
+  { name: "activity", location: "300",},
+  { name: "blog", location: "400",},
 ];
 
 menuIcons.forEach((element) => {
   element.addEventListener("click", (event) => {
     locationPages.forEach((pages) => {
       if (pages.name == event.target.id) {
-        sectionPages.forEach((element) => {
-          if (element.id == pages.name) {
-            sizePage(element);
-          }
-        });
         document.getElementById("titleSite").innerHTML = `Dashboard | ${pages.name}`;
         movePage(pages);
         menuIcons.forEach((element) => {
           element.classList.remove("active-icon");
         });
         element.classList.add("active-icon");
+
+        sectionPages.forEach((element) => {
+          if (element.id == pages.name) {
+            sizePage(element);
+          }
+        });
       }
     });
   });
@@ -38,6 +39,5 @@ function movePage(locationMove) {
 }
 
 function sizePage(size) {
-  console.log(size.children[0].clientHeight);
   parentPages.style.height = `${size.children[0].clientHeight + 100}px`;
 }
