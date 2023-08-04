89d7967fc40ad5d246411362180aeb4f1e6de3f8
Moein Mirsaeedi <moein.mirsaeedi@gmail.com>
2023-08-04 16:35:14 +0330

    Update

--- assets/js/pages/dashboard.js
+++ assets/js/pages/dashboard.js
@@ -2,7 +2,7 @@ let menuIcons = document.querySelectorAll(".icons-menu");
 let sectionPages = document.querySelectorAll(".sectionPages");
 
 let parentPages = document.getElementById("parentPages");
-parentPages.style.transform = `translateX(-200vw)`;
+parentPages.style.transform = `translateX(-300vw)`;
 parentPages.style.height = `870px`;
 
 let locationPages = [
@@ -41,3 +41,51 @@ function movePage(locationMove) {
 function sizePage(size) {
   parentPages.style.height = `${size.children[0].clientHeight + 100}px`;
 }
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Hong Kong', 'Macau', 'Japan', 'Switzerland', 'Spain', 'Singapore'],
+        datasets: [{
+            label: 'Life expectancy',
+            data: [78.308, 80.188, 84.118, 65.706, 82.5, 83.468],
+            backgroundColor: [
+                'rgba(216, 27, 96, 0.6)',
+                'rgba(3, 169, 244, 0.6)',
+                'rgba(255, 152, 0, 0.6)',
+                'rgba(29, 233, 182, 0.6)',
+                'rgba(156, 39, 176, 0.6)',
+                'rgba(84, 110, 122, 0.6)'
+            ],
+            borderColor: [
+                'rgba(216, 27, 96, 1)',
+                'rgba(3, 169, 244, 1)',
+                'rgba(255, 152, 0, 1)',
+                'rgba(29, 233, 182, 1)',
+                'rgba(156, 39, 176, 1)',
+                'rgba(84, 110, 122, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        title: {
+            display: true,
+            text: 'Life Expectancy by Country',
+            position: 'top',
+            fontSize: 16,
+            padding: 20
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    min: 60
+                }
+            }]
+        }
+    }
+});
